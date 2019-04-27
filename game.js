80025a92455d0045fce88745db78792af83aabb4
MarioCroSite <crositecom@gmail.com>
2019-04-27 11:52:43 +0200

    Immortal fixed...

--- game.js
+++ game.js
@@ -438,11 +438,13 @@ Ship = function () {
   this.collision = function (other) {
     SFX.explosion();
     Game.explosionAt(other.x, other.y);
-    Game.FSM.state = 'player_died';
+    /**
+	Game.FSM.state = 'player_died';
     this.visible = false;
+	**/
     this.currentNode.leave(this);
     this.currentNode = null;
-    Game.lives--;
+    /*Game.lives--;*/
   };
 
 };
