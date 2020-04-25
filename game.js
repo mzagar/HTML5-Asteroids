6ed8dda4a0b3b7cfe43acd5f35d6452741138474
Dario Protulipac <protulipac@gmail.com>
2020-04-25 14:41:33 +0200

    Immortality added

--- game.js
+++ game.js
@@ -436,13 +436,13 @@ Ship = function () {
   };
 
   this.collision = function (other) {
-    SFX.explosion();
+    // SFX.explosion();
     Game.explosionAt(other.x, other.y);
-    Game.FSM.state = 'player_died';
-    this.visible = false;
+    // Game.FSM.state = 'player_died';
+    // this.visible = false;
     this.currentNode.leave(this);
     this.currentNode = null;
-    Game.lives--;
+    // Game.lives--;
   };
 
 };
